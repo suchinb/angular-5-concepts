daadfe9433032a015baf57b2a1be6d8e96d9ef5b
Suchin Byreshappa <Suchin.Byreshappa@happiestminds.com>
2018-04-04 16:18:55 +0530

    template form missed files

---
+++ src/app/templateform/templateform.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Signup } from './../domain-models/signup';
+
+@Component({
+  selector: 'app-templateform',
+  templateUrl: './templateform.component.html',
+  styleUrls: ['./templateform.component.css']
+})
+export class TemplateformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    this.form= new Signup()
+  }
+  form:Signup;
+
+  submit() {
+    console.log(this.form)
+  }
+}
