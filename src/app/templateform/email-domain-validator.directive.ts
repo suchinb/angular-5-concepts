10ac98c6664f4670189d7e7b2dde8c95c31addf7
Suchin Byreshappa <Suchin.Byreshappa@happiestminds.com>
2018-04-09 21:36:52 +0530

    case insensitive domain validation

--- src/app/templateform/email-domain-validator.directive.ts
+++ src/app/templateform/email-domain-validator.directive.ts
@@ -5,7 +5,7 @@ function emailDomainValidator( control: FormControl ) {
   let email= control.value;
   if (email && email.indexOf('@') != -1) {
     let [_,domain] = email.split("@")
-    if (domain!=="gmail.com") {
+    if (domain.toLowerCase()!=="gmail.com") {
       return {
         emailDomain:{
           parsedDomain:domain
