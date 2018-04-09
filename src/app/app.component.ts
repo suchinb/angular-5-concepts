a8ca95d588fe60352731c42962621d9e1b0d0e1e
Suchin Byreshappa <Suchin.Byreshappa@happiestminds.com>
2018-04-09 21:34:49 +0530

    added scss feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { SideNavigationComponent } from './side-navigation/side-navigation.compo
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
